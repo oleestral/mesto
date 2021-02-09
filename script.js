eb8bb4429584ae3353fe9e1785d090f7c455d1b7
oleestral <less.pessimism@yandex.ru>
2021-02-09 13:28:09 +0300

    2 try

--- script.js
+++ script.js
@@ -1,43 +1,40 @@
-let popups = document.querySelector('.popups');
-let editButton = document.querySelector('.profile__info-editbutton');
-let popupOverlay = document.querySelector('.popup');
-let closeButton = document.querySelector('.popup__closeButton');
-
+/*popup*/
+let popup = document.querySelector('.popup');
+let popupOverlay = document.querySelector('.popup__containers');
+let closeButton = document.querySelector('.popup__close-button');
 let formElement = document.querySelector('.popup__input-container');
 let nameInput = document.querySelector('.popup__item_name')
 let captionInput = document.querySelector('.popup__item_caption')
+let submitButton = document.querySelector('.popup__submit-button');
 
+/*profile*/
+let editButton = document.querySelector('.profile__info-edit-button');
 let nameProfile = document.querySelector('.profile__info-name');
-let jobProfile = document.querySelector('.profile__info-selfdescription');
+let jobProfile = document.querySelector('.profile__info-self-description');
 
-let submitButton = document.querySelector('.popup__submitButton');
 
 /*открытие окна редактирования*/
 function editProfile () {
-    popups.classList.add('popup_opened');
+    popup.classList.add('popup_opened');
     popupOverlay.classList.add('popup_opened');
-    let name = nameProfile.textContent;
-    let job = jobProfile.textContent;
-
-    nameInput.value = name;
-    captionInput.value = job;
+    nameInput.value = nameProfile.textContent;;
+    captionInput.value = jobProfile.textContent;;
 }
-editButton.addEventListener('click',editProfile);
 
 /*закрытие окна редактирования*/
 function closeEditProfile () {
-    popups.classList.remove('popup_opened');
+    popup.classList.remove('popup_opened');
     popupOverlay.classList.remove('popup_opened');
 }
-closeButton.addEventListener('click', closeEditProfile);
-
+/*важная штука*/
 function formSubmitHandler(evt) {
     evt.preventDefault();
-
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = captionInput.value;
 
     closeEditProfile()
 }
+editButton.addEventListener('click',editProfile);
+closeButton.addEventListener('click', closeEditProfile);
+editButton.addEventListener('click',editProfile);
 formElement.addEventListener('submit', formSubmitHandler);
-submitButton.addEventListener('click', formSubmitHandler);
